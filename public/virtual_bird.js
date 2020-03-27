f307d7930839801c50f965380d295cbc7c033286
Denise <denise@Denises-MBP.home>
2020-03-27 23:03:41 +0100

    make it better

--- public/virtual_bird.js
+++ public/virtual_bird.js
@@ -300,4 +300,37 @@ var interaction = [
     }
 ];
 
+var $el = $("#scalable");
+var elHeight = $el.outerHeight();
+var elWidth = $el.outerWidth();
+
+var $wrapper = $("#wrapper");
+
+$wrapper.resizable({
+  resize: doResize
+});
+
+function doResize(event, ui) {
+  
+  var scale, origin;
+    
+  scale = Math.min(
+    ui.size.width / elWidth,    
+    ui.size.height / elHeight
+  );
+  
+  $el.css({
+    transform: "translate(-50%, -50%) " + "scale(" + scale + ")"
+  });
+  
+}
+
+var starterData = { 
+  size: {
+    width: $wrapper.width(),
+    height: $wrapper.height()
+  }
+}
+doResize(null, starterData);
+
 
